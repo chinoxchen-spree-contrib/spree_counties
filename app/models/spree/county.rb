f9b59a7229e1ae343f8a1584a0c4ffdeee682a43
rmartinezapp <robert.martinez.app@gmail.com>
2021-05-03 03:06:07 -0400

    add stock_location_ids to user_attributes

--- app/models/spree/county.rb
+++ app/models/spree/county.rb
@@ -2,7 +2,6 @@ class Spree::County < ActiveRecord::Base
   belongs_to :state
   has_many :addresses, dependent: :nullify
   has_many :stock_locations, dependent: :nullify
-  has_and_belongs_to_many :users
 
   validates :name, presence: true
 
